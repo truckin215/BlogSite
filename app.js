fd6c4bf3a1e539ce4101185bab3a7a74e661b5a6
Connor_Murray <connormurray215@gmail.com>
2019-07-09 02:47:31 -0400

    Finished Basic Functionality

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express')
+const mysql = require ('mysql')
 const bodyParser = require('body-parser')
+
 const urlEncoded = bodyParser.urlencoded({extended: false})
 
-const dummyData = [{title: "Work on my portfolio", blog: "Stuff" }];
 
-// setting up
+// setting up sql
 const app = express();
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'admin',
+    password : 'student',
+    database : 'Blog'
+    });
+
+db.connect(function(err){
+    if (err) throw err
+    console.log("My SQL is connected")
+})
+
 
 // setting template engine
 app.set("view engine","ejs");
@@ -18,27 +31,38 @@ app.use(express.static('./public'));
 // ############### ROUTES ##############
 
 // Get for tasks: returns all tasks
-app.get('/Post', function (req, res) {
-    console.log("hitting get route");
-    res.render('Post', {taskToDo: dummyData});
+app.get('/Posts', function (req, res) {
+    let sql = 'SELECT * FROM Post'
+    db.query(sql, function (err, results) {
+        if (err) throw err;
+        // rendering tasks view and passing taskToDo data
+        res.render('Posts', {blogPost: results});
+    });
 });
-
 // Post for tasks: posting a task
 app.post('/Post', urlEncoded, function(req, res){
   // console.log(req.body);
   let incomingItem = {}
   incomingItem.title = req.body.title;
-  incomingItem.blog = req.body.blog;
-  dummyData.push(incomingItem)
-  res.redirect('/Post')
+  incomingItem.post = req.body.blog;
+  console.log(incomingItem)
+  let sql = "INSERT INTO Post SET ?"
+    db.query(sql, incomingItem,(err, result) =>{
+        if(err) throw err;
+        console.log(result);
+        res.redirect('/Posts')
+    })
 });
 
-app.delete("/Post/:id", function(req, res){
-    // console.log(req.params.id);
-    dummyData.splice(req.params.id, 1);
-    // console.log(dummyData);
-    res.json(dummyData)
+app.delete("/Posts/:id", function(req, res){
+    let sql = 'DELETE FROM Post WHERE ID=' + req.params.id;
+    db.query(sql,(err, result) =>{
+        if(err) throw err;
+        console.log(result);
+        res.json(result)
+    })
 });
+
 
 app.listen(3000, function(err){
     if (err)
--- public/js/main.js
+++ public/js/main.js
@@ -2,14 +2,11 @@ const taskItems = document.getElementsByTagName('li');
 
 for (let i = 0; i < taskItems.length; i++) {
     const element = taskItems[i];
-    element.addEventListener('dblclick', function(){
-        fetch('/Post/' + element.id, {method: "delete"} )
-        .then(function(res){
-            // console.log(res.json());
-            return res.json()
-        })
-        .then(function(data){
-            location.reload();
-        })
+
+    element.addEventListener('click', function(){
+        console.log(element.id);
+        fetch('/Posts/' + element.id, { method: "delete" } )
+        .then( (res) => res.json() )
+        .then( (data) =>  location.reload() )
     }) 
 }
